4102f7004f462f317d8730e5d351d40eeb8deba2
YungSang <yungsang@gmail.com>
2011-07-17 18:11:23 -0700

    Separated sub functions for spar from _post()

--- model.gplus.js
+++ model.gplus.js
@@ -1,3 +1,12 @@
+/**
+ * Model.Google+ for Tombloo
+ *
+ * @version : 1.0.2
+ * @date    : 2011-07-17
+ * @author  : YungSang (http://topl.us/yungsang)
+ *
+ * [Tombloo]: https://github.com/to/tombloo/wiki
+ */
 models.register({
 	name     : 'Google+',
 	ICON     : 'http://ssl.gstatic.com/s2/oz/images/favicon.ico',
@@ -48,94 +57,152 @@ models.register({
 		return 'oz:' + oz[2][0] + '.' + Date.now().toString(16) + '.' + this.sequence.toString(16);
 	},
 
+	createLinkSpar : function(ps) {
+		if (ps.type == 'regular') {
+			return JSON.stringify([]);
+		}
+
+		var isYoutube = (ps.type == 'video' && ps.itemUrl.match(this.YOUTUBE_REGEX));
+		var videoUrl = '';
+		var imageUrl = '//s2.googleusercontent.com/s2/favicons?domain=' + createURI(ps.pageUrl).host;
+		if (isYoutube) {
+			videoUrl = ps.itemUrl.replace(this.YOUTUBE_REGEX,
+				'http://www.youtube.com/v/$1&hl=en&fs=1&autoplay=1');
+			imageUrl = ps.itemUrl.replace(this.YOUTUBE_REGEX,
+				'http://ytimg.googleusercontent.com/vi/$1/default.jpg');
+		}
+
+		var link = [];
+		link.push(
+			null, null, null,
+			ps.item || ps.page,
+			null,
+			isYoutube ? [null, videoUrl, 385, 640] : null,
+			null, null, null,
+			isYoutube ? [[null, ps.author, 'uploader']] : [],
+			null, null, null, null, null,
+			null, null, null, null, null, null,
+			ps.body,
+			null, null
+		);
+		switch (ps.type) {
+		case 'video':
+			link.push([null, ps.pageUrl, null, 'application/x-shockwave-flash', 'video']);
+			break;
+		case 'photo':
+			link.push([null, ps.pageUrl, null, 'text/html', 'document']);
+			break;
+		default:
+			link.push([null, ps.itemUrl || ps.pageUrl, null, 'text/html', 'document']);
+		}
+		link.push(
+			null, null, null, null, null,
+			null, null, null, null, null,
+			null, null, null, null, null, null,
+			[
+				[null, imageUrl, null, null],
+				[null, imageUrl, null, null]
+			],
+			null, null, null, null, null,
+			[[
+				null,
+				isYoutube ? 'youtube' : '',
+				'http://google.com/profiles/media/provider'
+			]]
+		);
+
+		return JSON.stringify(link);
+	},
+
+	craetePhotoSpar : function(ps) {
+		var mime = this.getMIMEType(ps.itemUrl);
+		return JSON.stringify([
+			null, null, null, null, null,
+			[null, ps.itemUrl],
+			null, null, null,
+			[],
+			null, null, null, null, null,
+			null, null, null, null, null,
+			null, null, null, null,
+			[
+				null, ps.pageUrl, null, mime, 'photo',
+				null, null, null, null, null, null, null, null, null
+			],
+			null, null, null, null, null,
+			null, null, null, null, null,
+			null, null, null, null, null, null,
+			[
+				[null, ps.itemUrl, null, null],
+				[null, ps.itemUrl, null, null]
+			],
+			null, null, null, null, null,
+			[
+				[null, 'images', 'http://google.com/profiles/media/provider']
+			]
+		]);
+	},
+
+	getMIMEType : function(url) {
+		switch (createURI(url).fileExtension) {
+		case 'bmp' : return('image/bmp');
+		case 'gif' : return('image/gif');
+		case 'jpeg': return('image/jpeg');
+		case 'jpg' : return('image/jpeg');
+		case 'png' : return('image/png');
+		}
+		return('image/jpeg');
+	},
+
+	createScopeSpar : function(oz) {
+		return JSON.stringify({
+			aclEntries : [
+				{
+					scope : {
+						scopeType   : "anyone",
+						name        :	"Anyone",
+						id          : "anyone",
+						me          : true,
+						requiresKey : false
+					},
+					role : 20
+				},
+				{
+					scope : {
+						scopeType   : "anyone",
+						name        : "Anyone",
+						id          : "anyone",
+						me          : true,
+						requiresKey : false
+					},
+					role : 60
+				}
+			]
+		});
+	},
+
 	_post : function(ps, oz) {
 		var self = this;
 
 		var spar = [];
-		spar.push(ps.description);
-		spar.push(this.getToken(oz));
-		spar.push(null,null,null,null);
+		spar.push(
+			(ps.type != 'regular') ? ps.description : joinText([ps.item, ps.description], "\n\n"),
+			this.getToken(oz),
+			null, null, null, null
+		);
 
-		var link = [];
-		link.push(null,null,null);
-		link.push(ps.item || ps.page);
-		link.push(null);
-		if (ps.type == 'video' && ps.itemUrl.match('youtube.com')) {
-			var videoUrl = ps.itemUrl.replace(this.YOUTUBE_REGEX,
-				'http://www.youtube.com/v/$1&hl=en&fs=1&autoplay=1');
-			link.push([null,videoUrl,385,640]);
-		}
-		else {
-			link.push(null);
-		}
-		link.push(null,null,null);
-		if (ps.type == 'video' && ps.itemUrl.match('youtube.com')) {
-			link.push([[null,ps.author,"uploader"]]);
-		}
-		else {
-			link.push([]);
-		}
-		link.push(null,null,null,null,null);
-		link.push(null,null,null,null,null,null);
-		link.push(ps.body);
-		link.push(null,null);
-		if (ps.type == 'video') {
-			link.push([null,ps.pageUrl,null,"application/x-shockwave-flash","video"]);
-		}
-		else if (ps.type == 'photo') {
-			link.push([null,ps.pageUrl,null,"text/html","document"]);
-		}
-		else {
-			link.push([null,ps.itemUrl || ps.pageUrl,null,"text/html","document"]);
-		}
-		link.push(null,null,null,null,null);
-		link.push(null,null,null,null,null);
-		link.push(null,null,null,null,null,null);
-		if (ps.type == 'video' && ps.itemUrl.match('youtube.com')) {
-			var imageUrl = ps.itemUrl.replace(this.YOUTUBE_REGEX,
-				'http://ytimg.googleusercontent.com/vi/$1/default.jpg');
-			link.push([[null,imageUrl,null,null],[null,imageUrl,null,null]]);
-		}
-		else {
-			var imageUrl = '//s2.googleusercontent.com/s2/favicons?domain=' + createURI(ps.pageUrl).host
-			link.push([[null,imageUrl,null,null],[null,imageUrl,null,null]]);
-		}
-		link.push(null,null,null,null,null);
-		if (ps.type == 'video' && ps.itemUrl.match('youtube.com')) {
-			link.push([[null,"youtube","http://google.com/profiles/media/provider"]]);
-		}
-		else {
-			link.push([[null,"","http://google.com/profiles/media/provider"]]);
-		}
-		link = JSON.stringify(link);
+		var link = this.createLinkSpar(ps);
+
 		if (ps.type == 'photo') {
-			var photo = [];
-			photo.push(null,null,null,null,null);
-			photo.push([null,ps.itemUrl]);
-			photo.push(null,null,null);
-			photo.push([]);
-			photo.push(null,null,null,null,null);
-			photo.push(null,null,null,null,null);
-			photo.push(null,null,null,null);
-
-			var mime = this.getMIMEType(ps.itemUrl);
-			photo.push([null,ps.pageUrl,null,mime,"photo",null,null,null,null,null,null,null,null,null]);
-			photo.push(null,null,null,null,null);
-			photo.push(null,null,null,null,null);
-			photo.push(null,null,null,null,null,null);
-			photo.push([[null,ps.itemUrl,null,null],[null,ps.itemUrl,null,null]]);
-			photo.push(null,null,null,null,null);
-			photo.push([[null,"images","http://google.com/profiles/media/provider"]]);
-			photo = JSON.stringify(photo);
-			spar.push(JSON.stringify([link,photo]));
+			var photo = this.craetePhotoSpar(ps);
+			spar.push(JSON.stringify([link, photo]));
 		}
 		else {
 			spar.push(JSON.stringify([link]));
 		}
 
 		spar.push(null);
-		spar.push("{\"aclEntries\":[{\"scope\":{\"scopeType\":\"anyone\",\"name\":\"Anyone\",\"id\":\"anyone\",\"me\":true,\"requiresKey\":false},\"role\":20},{\"scope\":{\"scopeType\":\"anyone\",\"name\":\"Anyone\",\"id\":\"anyone\",\"me\":true,\"requiresKey\":false},\"role\":60}]}");
-		spar.push(true,[],true,true,null,[],false,false);
+		spar.push(this.createScopeSpar(oz));
+		spar.push(true, [], true, true, null, [], false, false);
 
 		spar = JSON.stringify(spar);
 
@@ -152,16 +219,5 @@ models.register({
 		}).addCallback(function(res) {
 			return res.responseText;
 		});
-	},
-
-	getMIMEType : function(url) {
-		switch (createURI(url).fileExtension) {
-		case 'bmp' : return("image/bmp");
-		case 'gif' : return("image/gif");
-		case 'jpeg': return("image/jpeg");
-		case 'jpg' : return("image/jpeg");
-		case 'png' : return("image/png");
-		}
-		return("image/jpeg");
 	}
 });
