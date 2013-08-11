bf82a44ee2cf5669e3ec29a7441347380adfc7d3
YungSang <yungsang@gmail.com>
2013-08-11 15:06:21 -0700

    Follow the current method to upload an image

--- model.twitter-upload.js
+++ model.twitter-upload.js
@@ -1,17 +1,17 @@
 /**
  * Model.Twitter Upload for Tombloo
  *
- * @version : 1.2
- * @date    : 2011-11-20
+ * @version : 1.3
+ * @date    : 2013-08-11
  * @author  : YungSang (http://yungsang.com/+)
  *
  * [Tombloo]: https://github.com/to/tombloo/wiki
  */
 (function() {
-	const TWITTER_MODEL_NAME = 'Twitter';
+	var TWITTER_MODEL_NAME = 'Twitter';
 
 	addAround(models[TWITTER_MODEL_NAME], 'check', function(proceed, args) {
-		let ps = args[0], result = proceed(args);
+		var ps = args[0], result = proceed(args);
 		if (!result && ps) {
 			result = /(?:regular|photo|quote|link|conversation|video)/.test(ps.type);
 		}
@@ -19,7 +19,7 @@
 	});
 
 	addAround(models[TWITTER_MODEL_NAME], 'post', function(proceed, args, self) {
-		let ps = args[0];
+		var ps = args[0];
 		if (ps.type === 'photo') {
 			return self.download(ps).addCallback(function(file) {
 				return self.upload(ps, file);
@@ -34,7 +34,7 @@
 		},
 		upload : function(ps, file) {
 			var self = this;
-			var POST_URL = 'https://upload.twitter.com/1/statuses/update_with_media.json';
+			var POST_URL = 'https://upload.twitter.com/i/tweet/create_with_media.iframe';
 
 			var status = joinText([ps.description, (ps.body)? '"' + ps.body + '"' : '', ps.item, ps.pageUrl], ' ');
 
@@ -50,20 +50,22 @@
 					sendContent : {
 						status                  : status,
 						'media_data[]'          : btoa(bis.readBytes(file.fileSize)),
-						include_entities        : 'true',
+						iframe_callback         : 'window.top.swift_tweetbox_tombloo',
 						post_authenticity_token : token.authenticity_token
 					},
 					headers : {
-						Referer            : 'https://upload.twitter.com/receiver.html',
-						'X-Phx'            : true,
-						'X-Requested-With' : 'XMLHttpRequest'
+						Referer : self.URL
 					}
 				}).addCallback(function(res) {
-					var json = JSON.parse(res.responseText);
-					if (json.error) {
-						throw new Error(json.error);
-					}
-					return json;
+					var html = res.responseText;
+					var json = html.extract(/window.top.swift_tweetbox_tombloo\((\{.+\})\);/);
+					json = JSON.parse(json);
+				}).addErrback(function(e) {
+					var res  = e.message;
+					var html = res.responseText;
+					var json = html.extract(/window.top.swift_tweetbox_tombloo\((\{.+\})\);/);
+					json = JSON.parse(json);
+					throw new Error(json.error);
 				});
 			});
 		}
